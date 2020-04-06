8576ecb3468738168bab55f6ee15b6ea3a7800c2
Alex Stevens <zandra630@gmail.com>
2020-04-06 00:01:25 -0500

    Atarts README

--- middlewares/database.js
+++ middlewares/database.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose'
 
 // from https://hoangvvo.com/blog/migrate-from-express-js-to-next-js-api-routes/
 const database = handler => async (req, res) => {
-  console.log(process.env.MONGO_URI)
   if (mongoose.connections[0].readyState) return handler(req, res)
 
   await mongoose.connect(process.env.MONGO_URI, {
---
+++ pages/api/reports/shipping.js
@@ -0,0 +1,15 @@
+import database from '../../../middlewares/database.js'
+import Report from '../../../models/reports'
+
+const handler = (req, res) => {
+  console.log(req.body)
+  Report.find({})
+    .then(data => {
+      res.status(200).json(data)
+    })
+    .catch(e => {
+      console.log(e)
+      res.status(401).send('.... awkward 🐢')
+    })
+}
+export default database(handler)
